cff4140e14b2f32a07e6c53d836139dba99876f6
James Kulba <jkulba@gmail.com>
2021-05-31 15:19:35 -0700

    Enable button functions

--- contacts/model.py
+++ contacts/model.py
@@ -12,30 +12,40 @@ from PyQt6.QtSql import QSqlTableModel
 
 
 class ContactsModel:
-
     def __init__(self):
-
         self.model = self._createModel()
 
-
     @staticmethod
-
     def _createModel():
-
         """Create and set up the model."""
-
         tableModel = QSqlTableModel()
-
         tableModel.setTable("contacts")
-
         tableModel.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange)
-
         tableModel.select()
-
         headers = ("ID", "Name", "Job", "Email")
-
         for columnIndex, header in enumerate(headers):
-
             tableModel.setHeaderData(columnIndex, Qt.Orientation.Horizontal, header)
-
         return tableModel
+
+    def addContact(self, data):
+        """Add a contact to the database."""
+        rows = self.model.rowCount()
+        self.model.insertRows(rows, 1)
+        for column_index, field in enumerate(data):
+            self.model.setData(self.model.index(rows, column_index + 1), field)
+        self.model.submitAll()
+        self.model.select()
+
+    def deleteContact(self, row):
+        """Remove a contact from the database."""
+        self.model.removeRow(row)
+        self.model.submitAll()
+        self.model.select()
+
+    def clearContacts(self):
+        """Remove all contacts in the database."""
+        self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnManualSubmit)
+        self.model.removeRows(0, self.model.rowCount())
+        self.model.submitAll()
+        self.model.setEditStrategy(QSqlTableModel.EditStrategy.OnFieldChange)
+        self.model.select()
--- contacts/views.py
+++ contacts/views.py
@@ -2,10 +2,16 @@
 
 """This module provides views to manage the contacts table."""
 
+from PyQt6.QtCore import Qt
 from PyQt6.QtWidgets import (
     QAbstractItemView,
+    QDialog,
+    QDialogButtonBox,
+    QFormLayout,
     QHBoxLayout,
+    QLineEdit,
     QMainWindow,
+    QMessageBox,
     QPushButton,
     QTableView,
     QVBoxLayout,
@@ -40,8 +46,11 @@ class Window(QMainWindow):
         self.table.resizeColumnsToContents()
         # Create buttons
         self.addButton = QPushButton("Add...")
+        self.addButton.clicked.connect(self.openAddDialog)
         self.deleteButton = QPushButton("Delete")
+        self.deleteButton.clicked.connect(self.deleteContact)
         self.clearAllButton = QPushButton("Clear All")
+        self.clearAllButton.clicked.connect(self.clearContacts)
         # Lay out the GUI
         layout = QVBoxLayout()
         layout.addWidget(self.addButton)
@@ -50,3 +59,99 @@ class Window(QMainWindow):
         layout.addWidget(self.clearAllButton)
         self.layout.addWidget(self.table)
         self.layout.addLayout(layout)
+
+    def openAddDialog(self):
+
+        """Open the Add Contact dialog."""
+
+        dialog = AddDialog(self)
+
+        if dialog.exec() == QDialog.DialogCode.Accepted:
+            self.contactsModel.addContact(dialog.data)
+            self.table.resizeColumnsToContents()
+
+    def deleteContact(self):
+        """Delete the selected contact from the database."""
+        row = self.table.currentIndex().row()
+        if row < 0:
+            return
+
+        messageBox = QMessageBox.warning(
+            self,
+            "Warning!",
+            "Do you want to remove the selected contact?",
+            QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel,
+        )
+
+        if messageBox == QMessageBox.StandardButton.Ok:
+            self.contactsModel.deleteContact(row)
+
+    def clearContacts(self):
+        """Remove all contacts from the database."""
+        messageBox = QMessageBox.warning(
+            self,
+            "Warning!",
+            "Do you want to remove all your contacts?",
+            QMessageBox.StandardButton.Ok | QMessageBox.StandardButton.Cancel,
+        )
+
+        if messageBox == QMessageBox.StandardButton.Ok:
+            self.contactsModel.clearContacts()
+
+class AddDialog(QDialog):
+    """Add Contact dialog."""
+
+    def __init__(self, parent=None):
+        """Initializer."""
+        super().__init__(parent=parent)
+        self.setWindowTitle("Add Contact")
+        self.layout = QVBoxLayout()
+        self.setLayout(self.layout)
+        self.data = None
+
+        self.setupUI()
+
+    def setupUI(self):
+        """Setup the Add Contact dialog's GUI."""
+        # Create line edits for data fields
+        self.nameField = QLineEdit()
+        self.nameField.setObjectName("Name")
+        self.jobField = QLineEdit()
+        self.jobField.setObjectName("Job")
+        self.emailField = QLineEdit()
+        self.emailField.setObjectName("Email")
+        # Lay out the data fields
+        layout = QFormLayout()
+        layout.addRow("Name:", self.nameField)
+        layout.addRow("Job:", self.jobField)
+        layout.addRow("Email:", self.emailField)
+        self.layout.addLayout(layout)
+        # Add standard buttons to the dialog and connect them
+        self.buttonsBox = QDialogButtonBox(self)
+        self.buttonsBox.setOrientation(Qt.Orientation.Horizontal)
+        self.buttonsBox.setStandardButtons(
+            QDialogButtonBox.StandardButton.Ok | QDialogButtonBox.StandardButton.Cancel
+        )
+        self.buttonsBox.accepted.connect(self.accept)
+        self.buttonsBox.rejected.connect(self.reject)
+        self.layout.addWidget(self.buttonsBox)
+
+    def accept(self):
+        """Accept the data provided through the dialog."""
+        self.data = []
+        for field in (self.nameField, self.jobField, self.emailField):
+            if not field.text():
+                QMessageBox.critical(
+                    self,
+                    "Error!",
+                    f"You must provide a contact's {field.objectName()}",
+                )
+                self.data = None  # Reset .data
+                return
+
+            self.data.append(field.text())
+
+        if not self.data:
+            return
+
+        super().accept()
